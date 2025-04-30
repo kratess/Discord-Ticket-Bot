424146e175dd65c98f87235305fb66d67bf12ef9
kratess <parvudoruionut@yahoo.ro>
2025-04-30 15:19:43 +0200

    upd files ignore

--- src/data.ts
+++ src/data.ts
@@ -1,6 +1,6 @@
 export default {
-  guildId: "",
-  channelId: "",
+  guildId: "964914548391952395",
+  channelId: "1366918674233032745",
 
   message: {
     title: "🎟️ Ticket Dashboard",
@@ -19,17 +19,17 @@ export default {
     {
       name: "🛠️ Support",
       desc: "Request support for general issues",
-      categoryId: "",
+      categoryId: "1367076395552866375",
     },
     {
       name: "⛔ Ban",
       desc: "Open a ticket if you believe your ban is unfair",
-      categoryId: "",
+      categoryId: "1367076527367258112",
     },
     {
       name: "💸 Donations",
       desc: "Open a ticket to make a donation",
-      categoryId: "",
+      categoryId: "1367076544492867584",
     },
   ],
 
@@ -43,7 +43,7 @@ export default {
   },
 
   transcript: {
-    channelId: "",
+    channelId: "1367088362732785705",
     limit: 100, // limit of messages
     message: "📄 Transcript of the **#${ticket}** ticket closed by ${user}",
   },
